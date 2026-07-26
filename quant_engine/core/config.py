33cdde116f44f3e56eb8007227643bcd487400dc
rkovath-netizen <rkovath@gmail.com>
2026-07-26 21:54:56 +0530

    Update config.py

--- quant_engine/core/config.py
+++ quant_engine/core/config.py
@@ -1,1 +1,15 @@
+import pytz
+
+IST = pytz.timezone('Asia/Kolkata')
+
+TIMEFRAME_COMBOS = [
+    ('3min', '15min'),
+    ('5min', '30min'),
+    ('10min', '60min')
+]
+
+INDEX_CONFIG = {
+    "NIFTY": {"underlying": "NSE_INDEX|Nifty 50", "step": 50, "segment": "NSE_FO"},
+    "SENSEX": {"underlying": "BSE_INDEX|SENSEX", "step": 100, "segment": "BSE_FO"}
+}
 
